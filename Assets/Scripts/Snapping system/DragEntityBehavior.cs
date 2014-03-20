53336e4cb41acd3c7c01d47ecefb673cf20a5384
Ernestas Juška <ernestas.juska@ktu.edu>
2014-03-20 18:30:36 +0200

    snap-1

---
+++ Assets/Scripts/Snapping system/DragEntityBehavior.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragEntityBehavior : MonoBehaviour {
+
+	Transform grabbed;
+	public float grabDistance = 10.0f;
+	
+	public bool useToggleDrag = true; // Didn't know which style you prefer. 
+	
+	void Update () {
+		if (useToggleDrag)
+			UpdateToggleDrag();
+		else
+			UpdateHoldDrag();
+	}
+
+	// Toggles drag with mouse click
+	void UpdateToggleDrag () {
+		if (Input.GetMouseButtonDown(0)) 
+			Grab();
+		else if (grabbed)
+			Drag();
+	}
+	
+	// Drags when user holds down button
+	void UpdateHoldDrag () {
+		if (Input.GetMouseButton(0)) 
+			if (grabbed)
+				Drag();
+			else 
+				Grab();
+		else
+			grabbed = null;
+	}
+	
+	void Grab() {
+		if (grabbed) 
+			grabbed = null;
+		else {
+			RaycastHit hit;
+			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit))          
+				grabbed = hit.transform;
+		}
+	}
+	
+	void Drag() {
+		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		var position = transform.position + transform.forward * grabDistance;
+		var plane = new Plane(-transform.forward, position);
+		float distance;
+		if (plane.Raycast(ray, out distance)) {
+			grabbed.position = ray.origin + ray.direction * distance;
+			grabbed.rotation = transform.rotation;
+		}
+	}
+}
---
+++ Assets/Scripts/Snapping system/StickyEntityBehavior.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Better switch to a global script
+// or global script to start dragging
+
+public class StickyEntityBehavior : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+
+	}
+
+	void OnMouseDown() {
+		var ray = cam.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit)) {
+			if (hit.collider.gameObject == gameObject) {
+				_isGrabbed = true;
+				hint.rigidbody.MovePosition(new Vector3(0, gameObject.transform.position.y, 0));
+				var q = new Quaternion();
+				q.SetLookRotation(cam.transform.forward);
+				hint.rigidbody.MoveRotation(q);
+				_plane = new Plane(-cam.transform.forward, new Vector3(0, gameObject.transform.position.y, 0));
+			}
+		}
+	}
+	
+	void OnMouseDrag() {
+		if (_isGrabbed) {
+			var ray = cam.ScreenPointToRay(Input.mousePosition);
+
+			gameObject.rigidbody.velocity = Vector3.zero;
+			gameObject.rigidbody.angularVelocity = Vector3.zero;
+			gameObject.collider.enabled = false;
+
+			RaycastHit hit;
+			float enter;
+			if (Physics.Raycast(ray, out hit)) {
+				gameObject.rigidbody.MovePosition(radialSnappoint.transform.localPosition.magnitude * hit.normal + hit.point);
+				var q = new Quaternion();
+				q.SetLookRotation(hit.normal);
+				gameObject.rigidbody.MoveRotation(q);
+			} 
+			else if (_plane.Raycast(ray, out enter)) {
+				var point = ray.origin + ray.direction * enter;
+				gameObject.transform.position = point;
+
+				gameObject.transform.rotation = Quaternion.identity;
+			}
+
+			gameObject.collider.enabled = true;
+		}
+	}
+
+	public bool radialSnapping = true;
+	public Camera cam;
+	public GameObject hint;
+	public GameObject radialSnappoint;
+
+	private bool _isGrabbed = false;
+	private Plane _plane;
+}
